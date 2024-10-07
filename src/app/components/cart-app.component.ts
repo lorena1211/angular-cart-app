d4b832fbe8cca9b795ae443bb58023e44b20bc58
Lorena Naranjo Arias <lorenaran12@gmail.com>
2024-10-07 01:18:57 -0500

    Se agregan alertas y rutas

--- src/app/components/cart-app.component.ts
+++ src/app/components/cart-app.component.ts
@@ -1,20 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
-import { Product } from '../models/product';
 import { CatalogComponent } from './catalog/catalog.component';
 import { CartItem } from '../models/cartItem';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import { Router, RouterOutlet } from '@angular/router';
+import { SharingDataService } from '../services/sharing-data.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'cart-app',
   standalone: true,
-  imports: [CatalogComponent, NavbarComponent],
+  imports: [CatalogComponent, NavbarComponent, RouterOutlet],
   templateUrl: './cart-app.component.html'
 })
 export class CartAppComponent implements OnInit{
-
-  products: Product[] = [];
 
   items: CartItem[] = [];
 
@@ -22,43 +21,83 @@ export class CartAppComponent implements OnInit{
 
   showCart: boolean = false;
 
-  constructor(private service: ProductService){
+  constructor(
+    private service: ProductService, 
+    private sharingDataService: SharingDataService,
+    private router: Router
+  ){
 
   }
   ngOnInit(): void {
-    this.products = this.service.findAll();
     this.items = JSON.parse(sessionStorage.getItem('cart')!) || [];
     this.calculateTotal();
+    this.onDeleteCart();
+    this.onAddCart();
   }
 
-  onAddCart(product: Product): void {
-    const hasItem = this.items.find(item => item.product.id === product.id);
-
-    if (hasItem) {
-      this.items = this.items.map( item => {
-        if (item.product.id === product.id) {
-          return {
-            ...item,
-            quantity: item.quantity + 1
+  onAddCart(): void {
+    this.sharingDataService.productEventEmitter.subscribe(product => {
+      const hasItem = this.items.find(item => item.product.id === product.id);
+  
+      if (hasItem) {
+        this.items = this.items.map( item => {
+          if (item.product.id === product.id) {
+            return {
+              ...item,
+              quantity: item.quantity + 1
+            }
           }
-        }
-        return item;
+          return item;
+        })
+      } else {
+        this.items = [... this.items, {product: { ...product}, quantity:1}];
+      }
+      this.calculateTotal();
+      this.saveSession();
+      this.router.navigate(['/cart'], {
+        state: {items: this.items, total: this.total}
       })
-    } else {
-      this.items = [... this.items, {product: { ...product}, quantity:1}];
-    }
-    this.calculateTotal();
-    this.saveSession();
+      Swal.fire({
+        title: "Shopping Cart",
+        text: "Nuevo producto agregado al carrito de compras!",
+        icon: "success"
+      });
+    });
   }
 
-  onDeleteCart(id: number): void {
-    this.items = this.items.filter(item => item.product.id !== id);
-    if(this.items.length == 0) {
-      sessionStorage.removeItem('cart');
-      sessionStorage.clear();
-    }
-    this.calculateTotal();
-    this.saveSession();
+  onDeleteCart(): void {
+    this.sharingDataService.idProductEventeEmitter.subscribe( id => {
+      Swal.fire({
+        title: "Está seguro que desea eliminar el producto?",
+        text: "Cuidado!, esta acción es irreversible",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Si, eliminar"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.items = this.items.filter(item => item.product.id !== id);
+          if(this.items.length == 0) {
+            sessionStorage.removeItem('cart');
+            sessionStorage.clear();
+          }
+          this.calculateTotal();
+          this.saveSession();
+          this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+            this.router.navigate(['/cart'], {
+              state: {items: this.items, total: this.total}
+            })
+          })
+          Swal.fire({
+            title: "Eliminado!",
+            text: "El producto ha sido eliminado del carrito de compras",
+            icon: "success"
+          });
+        }
+      });
+      
+    });
   }
 
   calculateTotal(): void {
--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter } from '@angular/core';
 import { CartItem } from '../../models/cartItem';
+import { Router } from '@angular/router';
+import { SharingDataService } from '../../services/sharing-data.service';
 
 @Component({
   selector: 'cart',
@@ -12,10 +14,14 @@ export class CartComponent {
   items: CartItem[] = [];
   total = 0;
   
-  idProductEventEmitter = new EventEmitter();
-  
+  constructor(private router: Router, private sharingDataService: SharingDataService) {
+    this.items = this.router.getCurrentNavigation()?.extras.state!['items'];
+    this.total = this.router.getCurrentNavigation()?.extras.state!['total'];
+
+  }
+
   onDeleteCart(id: number) {
-    this.idProductEventEmitter.emit(id);
+    this.sharingDataService.idProductEventeEmitter.emit(id);
   }
 
 }

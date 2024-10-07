3bf3a08eff50e55a362043656be51a39f269c2f6
Lorena Naranjo Arias <lorenaran12@gmail.com>
2024-10-06 22:13:38 -0500

    Shopping cart app

---
+++ src/app/data/product.data.ts
@@ -0,0 +1,40 @@
+import { Product } from "../models/product";
+
+export const products: Product[] = [
+    {
+        id: 1,
+        name: 'Teclado mecanico Asus RGB',
+        description: 'Algún teclado mecanico con luces RGB cherry red',
+        price: 1000
+    },
+    {
+        id: 2,
+        name: 'Samsung Smart TV LED 75',
+        description: 'Algún LCD excelente OLED',
+        price: 2000
+    },
+    {
+        id: 3,
+        name: 'Sony Camara Video',
+        description: 'Alguna camara de video para streaming',
+        price: 2300
+    },
+    {
+        id: 4,
+        name: 'Corsair memorias ram 16 GB DDR5',
+        description: 'Memoria ram optimizada para video juegos',
+        price: 3400
+    },
+    {
+        id: 5,
+        name: 'Nvidia ASUS RTX4900',
+        description: 'Tarjeta de videoNvidia optimizada para tareas multicore video de juegos en 4K',
+        price: 4500
+    },
+    {
+        id: 6,
+        name: 'CPU Intel core i7',
+        description: 'CPU optimizadas para tareas multicore',
+        price: 6000
+    }
+]
